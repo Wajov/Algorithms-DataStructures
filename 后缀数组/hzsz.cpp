6dcb6342536f65ad1c6eb11d337b01dfac988408
Wang Anjie <waj19971112@qq.com>
2017-08-28 01:19:35 +0800

    Simplified suffix array

--- 后缀数组/hzsz.cpp
+++ 后缀数组/hzsz.cpp
@@ -2,20 +2,25 @@
 using namespace std;
 int n, a[100001], b[100001], sum[100001], tmp[100001], id[100001], rk[100010];
 char s[100010];
-inline bool cmp(int x, int y)
+void Sort(int a[], int m)
 {
-    return s[x] < s[y];
+    memset(sum, 0, sizeof(sum));
+    for (int i = 1; i <= n; i++)
+        sum[a[i]]++;
+    for (int i = 1; i <= m; i++)
+        sum[i] += sum[i - 1];
+    for (int i = n; i; i--)
+        tmp[id[i]] = sum[a[id[i]]]--;
+    for (int i = 1; i <= n; i++)
+        id[tmp[i]] = i;
 }
 int main()
 {
     scanf("%s", s + 1);
     n = strlen(s + 1);
     for (int i = 1; i <= n; i++)
-    {
-        a[i] = b[i] = s[i];
-        id[i] = i;
-    }
-    sort(id + 1, id + n + 1, cmp);
+        a[id[i] = i] = s[i] - 97;
+    Sort(a, 25);
     for (int i = 1; i <= n; i <<= 1)
     {
         for (int j = 1, t = 0; j <= n; j++)
@@ -24,23 +29,9 @@ int main()
         {
             a[j] = rk[j];
             b[j] = rk[min(i + j, n + 1)];
-            id[j] = j;
         }
-        for (int j = 0; j < 2; j++)
-        {
-            memset(sum, 0, sizeof(sum));
-            for (int k = 1; k <= n; k++)
-                sum[b[k]]++;
-            for (int k = 1; k <= n; k++)
-                sum[k] += sum[k - 1];
-            for (int k = n; k; k--)
-            {
-                tmp[id[k]] = sum[b[id[k]]]--;
-                swap(a[id[k]], b[id[k]]);
-            }
-            for (int k = 1; k <= n; k++)
-                id[tmp[k]] = k;
-        }
+        Sort(b, n);
+        Sort(a, n);
     }
     for (int i = 1; i < n; i++)
         printf("%d ", rk[i]);
