3a02108e23678bd8c643aa07457745d08ceac162
Wang Anjie <waj19971112@qq.com>
2017-08-28 00:56:09 +0800

    Optimized suffix array

--- 后缀数组/hzsz.cpp
+++ 后缀数组/hzsz.cpp
@@ -2,49 +2,50 @@
 #define x first
 #define y second
 using namespace std;
-int n, key[100001], idx[100001], sum[100001], Rank[100010];
+int n, a[100001], b[100001], sum[100001], tmp[100001], id[100001], rk[100010];
 char s[100010];
-pair<pair<int, int>, int> tmp[100001], temp[100001];
-void Sort()
+inline bool cmp(int x, int y)
 {
-    for (int i = 1; i <= n; i++)
-        key[i] = i;
-    for (int i = 0; i < 2; i++)
-    {
-        memset(sum, 0, sizeof(sum));
-        for (int i = 1; i <= n; i++)
-            sum[tmp[i].x.y]++;
-        for (int i = 1; i <= max(n, 130); i++)
-            sum[i] += sum[i - 1];
-        for (int i = 1; i <= n; i++)
-            idx[key[i]] = i;
-        for (int i = n; i; i--)
-        {
-            key[idx[i]] = sum[tmp[idx[i]].x.y]--;
-            swap(tmp[idx[i]].x.x, tmp[idx[i]].x.y);
-        }
-    }
-    for (int i = 1; i <= n; i++)
-        temp[key[i]] = tmp[i];
-    memcpy(tmp, temp, sizeof(tmp));
+    return s[x] < s[y];
 }
 int main()
 {
     scanf("%s", s + 1);
     n = strlen(s + 1);
     for (int i = 1; i <= n; i++)
-        tmp[i] = {{s[i], s[i]}, i};
-    Sort();
+    {
+        a[i] = b[i] = s[i];
+        id[i] = i;
+    }
+    sort(id + 1, id + n + 1, cmp);
     for (int i = 1; i <= n; i <<= 1)
     {
         for (int j = 1, t = 0; j <= n; j++)
-            Rank[tmp[j].y] = tmp[j].x == tmp[j - 1].x ? t : ++t;
+            rk[id[j]] = a[id[j]] == a[id[j - 1]] && b[id[j]] == b[id[j - 1]] ? t : ++t;
         for (int j = 1; j <= n; j++)
-            tmp[j] = {{Rank[j], Rank[min(i + j, n + 1)]}, j};
-        Sort();
+        {
+            a[j] = rk[j];
+            b[j] = rk[min(i + j, n + 1)];
+            id[j] = j;
+        }
+        for (int j = 0; j < 2; j++)
+        {
+            memset(sum, 0, sizeof(sum));
+            for (int k = 1; k <= n; k++)
+                sum[b[k]]++;
+            for (int k = 1; k <= n; k++)
+                sum[k] += sum[k - 1];
+            for (int k = n; k; k--)
+            {
+                tmp[id[k]] = sum[b[id[k]]]--;
+                swap(a[id[k]], b[id[k]]);
+            }
+            for (int k = 1; k <= n; k++)
+                id[tmp[k]] = k;
+        }
     }
     for (int i = 1; i < n; i++)
-        printf("%d ", Rank[i]);
-    printf("%d\n", Rank[n]);
+        printf("%d ", rk[i]);
+    printf("%d\n", rk[n]);
     return 0;
 }
