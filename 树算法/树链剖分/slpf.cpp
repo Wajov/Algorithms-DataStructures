0cf7bcbf2d001875c1d7af79a4e4671c8b2411c5
Wang Anjie <waj19971112@qq.com>
2017-09-08 18:34:00 +0800

    Simplified heavy path decomposition

--- 树算法/树链剖分/slpf.cpp
+++ 树算法/树链剖分/slpf.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 const int N = 100001;
-int n, u, v, tot, num, a[N], d[N], f[N], s[N], son[N], top[N], idx[N], Head[N], Next[N << 1], Link[N << 1], sum[N];
+int n, m, u, v, tot, num, d[N], f[N], s[N], son[N], top[N], idx[N], key[N], Head[N], Next[N << 1], Link[N << 1];
 inline void AddEdge(int u, int v)
 {
     Next[++tot] = Head[u];
@@ -25,7 +25,7 @@ void DFS1(int x)
 void DFS2(int x)
 {
     top[x] = x == son[f[x]] ? top[f[x]] : x;
-    idx[x] = ++num;
+    key[idx[x] = ++num] = x;
     if (son[x])
         DFS2(son[x]);
     for (int i = Head[x], j; i; i = Next[i])
@@ -35,45 +35,21 @@ void DFS2(int x)
             DFS2(j);
     }
 }
-void TAdd(int x, int y)
+int LCA(int x, int y)
 {
-    for (; x <= n; x += x & -x)
-        sum[x] += y;
-}
-int TSum(int x)
-{
-    int ans = 0;
-    for (; x; x -= x & -x)
-        ans += sum[x];
-    return ans;
-}
-void Add(int x, int y)
-{
-    TAdd(idx[x], y);
-}
-int Sum(int x, int y)
-{
-    int u, v, ans = 0;
+    int u, v;
     while ((u = top[x]) != (v = top[y]))
         if (d[u] > d[v])
-        {
-            ans += TSum(idx[x]) - TSum(idx[u] - 1);
             x = f[u];
-        }
         else
-        {
-            ans += TSum(idx[y]) - TSum(idx[v] - 1);
             y = f[v];
-        }
     if (d[x] > d[y])
         swap(x, y);
-    return ans += TSum(idx[y]) - TSum(idx[x] - 1);
+    return x;
 }
 int main()
 {
     scanf("%d", &n);
-    for (int i = 1; i <= n; i++)
-        scanf("%d", &a[i]);
     for (int i = 1; i < n; i++)
     {
         scanf("%d%d", &u, &v);
@@ -82,5 +58,11 @@ int main()
     }
     DFS1(1);
     DFS2(1);
+    scanf("%d", &m);
+    for (int i = 1; i <= m; i++)
+    {
+        scanf("%d%d", &u, &v);
+        printf("%d\n", LCA(u, v));
+    }
     return 0;
 }
