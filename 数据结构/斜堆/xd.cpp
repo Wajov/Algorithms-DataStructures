f77294151870b0475e2378ef52c57bd9988649c5
Wang Anjie <waj19971112@qq.com>
2017-10-28 10:59:35 +0800

    Updated

--- 数据结构/斜堆/xd.cpp
+++ 数据结构/斜堆/xd.cpp
@@ -1,23 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 const int N = 100001;
-int l[N], r[N], key[N];
+int num, l[N], r[N], key[N];
 int Merge(int p, int q) {
     if (!p)
         return q;
     if (!q)
         return p;
-    if (key[p] < key[q])
+    if (key[p] > key[q])
         swap(p, q);
     r[p] = Merge(r[p], q);
     swap(l[p], r[p]);
     return p;
 }
-int Top(int p) {
-    return key[p];
+void Push(int &p, int x) {
+    key[++num] = x;
+    p = Merge(p, num);
 }
 void Pop(int &p) {
     p = Merge(l[p], r[p]);
+}
+int Top(int p) {
+    return key[p];
 }
 int main() {
     return 0;
